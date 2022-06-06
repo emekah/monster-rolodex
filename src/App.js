3e71d78b42b0104f0692541721c98c46e5b8c3b5
Emeka Osuji <emekah@emekas-mbp-2.home>
2022-06-06 10:54:53 +0100

    input state corrected

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
@@ -22,6 +23,9 @@ class App extends Component {
   }
 
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
     return (
       <div className="App">
         <input
@@ -29,24 +33,21 @@ class App extends Component {
           type="search"
           placeholder="search monsters"
           onChange={(event) => {
-            console.log(event.target.value);
-            const searchString = event.target.value.toLocaleLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString);
-            });
+            
+            const searchField = event.target.value.toLocaleLowerCase();
+
             this.setState(() => {
-              return { monsters: filteredMonsters };
-            })
+              return { searchField };
+            });
           }}
         />
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
-          )
+          );
         })}
-        
       </div>
     );
   }
